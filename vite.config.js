2a96d48f5511433c054a64fcdb6c9fc41e7a7047
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-05-19 19:59:28 +0200

    added base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import elm from "vite-plugin-elm";
 
 export default defineConfig({
   root: "src",
+  base: "https://aminnairi.github.io/package-json-generator",
   server: {
     port: process.env.VITE_SERVER_PORT,
     host: process.env.VITE_SERVER_HOST
